fa3ba1a1b9d048ce6c65be14ce467816ef4bc785
DisconnectRZN <36986483+DisconnectRZN@users.noreply.github.com>
2018-04-09 17:10:22 +0300

    Задание №3: Сделать всё то же самое, но для контактов
    Тоже задание что и для групп, только для создания контактов

--- New_Update_Contact(AddressBook).py
+++ New_Update_Contact(AddressBook).py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
+from contact import Contact
 
 class NewUpdateContactAddressBook(unittest.TestCase):
     def setUp(self):
@@ -18,9 +17,12 @@ class NewUpdateContactAddressBook(unittest.TestCase):
     def test_new_update_contact_address_book(self):
         driver = self.driver
         self.open_site(driver)
-        self.login(driver)
+        self.login(driver, username="admin", password="secret")
         self.open_new_contact_creation_form(driver)
-        self.create_new_contact(driver)
+        self.create_new_contact(driver, Contact(first="First name", middle="Middle name", last="Last name", nick="Nickname", title="Title", company="Company",
+                                address="Address", home_telephone="Home telephone", mobile_telephone="Mobile telephone", work_telephone="Work telephone", fax_telephone="Fax telephone",
+                                email_1="Email 1", email_2="Email 2", email_3="Email 3", home_page="Homepage", bday="1", bmonth="January", byear="1980", aday="18", amonth="July",
+                                ayear="1998", address_2="Address", home_address="Home address", notes="Notes"))
         self.return_to_home_page(driver)
         self.logout(driver)
 
@@ -32,66 +34,66 @@ class NewUpdateContactAddressBook(unittest.TestCase):
         # return to home page
         driver.find_element_by_link_text("home page").click()
 
-    def create_new_contact(self, driver):
+    def create_new_contact(self, driver, contact):
         # fill new contact form
         driver.find_element_by_name("firstname").click()
         driver.find_element_by_name("firstname").clear()
-        driver.find_element_by_name("firstname").send_keys("First name")
+        driver.find_element_by_name("firstname").send_keys(contact.first)
         driver.find_element_by_name("middlename").clear()
-        driver.find_element_by_name("middlename").send_keys("Middle name")
+        driver.find_element_by_name("middlename").send_keys(contact.middle)
         driver.find_element_by_name("lastname").clear()
-        driver.find_element_by_name("lastname").send_keys("Last name")
+        driver.find_element_by_name("lastname").send_keys(contact.last)
         driver.find_element_by_name("nickname").clear()
-        driver.find_element_by_name("nickname").send_keys("Nickname")
+        driver.find_element_by_name("nickname").send_keys(contact.nick)
         driver.find_element_by_name("title").clear()
-        driver.find_element_by_name("title").send_keys("Title")
+        driver.find_element_by_name("title").send_keys(contact.title)
         driver.find_element_by_name("company").clear()
-        driver.find_element_by_name("company").send_keys("Company")
+        driver.find_element_by_name("company").send_keys(contact.company)
         driver.find_element_by_name("address").clear()
-        driver.find_element_by_name("address").send_keys("Address")
+        driver.find_element_by_name("address").send_keys(contact.address)
         driver.find_element_by_name("home").clear()
-        driver.find_element_by_name("home").send_keys("Home telephone")
+        driver.find_element_by_name("home").send_keys(contact.home_telephone)
         driver.find_element_by_name("mobile").clear()
-        driver.find_element_by_name("mobile").send_keys("Mobile telephone")
+        driver.find_element_by_name("mobile").send_keys(contact.mobile_telephone)
         driver.find_element_by_name("work").clear()
-        driver.find_element_by_name("work").send_keys("Work telephone")
+        driver.find_element_by_name("work").send_keys(contact.work_telephone)
         driver.find_element_by_name("fax").clear()
-        driver.find_element_by_name("fax").send_keys("Fax telephone")
+        driver.find_element_by_name("fax").send_keys(contact.fax_telephone)
         driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys("Email 1")
+        driver.find_element_by_name("email").send_keys(contact.email_1)
         driver.find_element_by_name("email2").clear()
-        driver.find_element_by_name("email2").send_keys("Email 2")
+        driver.find_element_by_name("email2").send_keys(contact.email_2)
         driver.find_element_by_name("email3").clear()
-        driver.find_element_by_name("email3").send_keys("Email 3")
+        driver.find_element_by_name("email3").send_keys(contact.email_3)
         driver.find_element_by_name("homepage").clear()
-        driver.find_element_by_name("homepage").send_keys("Homepage")
+        driver.find_element_by_name("homepage").send_keys(contact.home_page)
         driver.find_element_by_name("bday").click()
-        Select(driver.find_element_by_name("bday")).select_by_visible_text("1")
+        Select(driver.find_element_by_name("bday")).select_by_visible_text(contact.bday)
         driver.find_element_by_name("bday").click()
         driver.find_element_by_name("bmonth").click()
-        Select(driver.find_element_by_name("bmonth")).select_by_visible_text("January")
+        Select(driver.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
         driver.find_element_by_name("bmonth").click()
         driver.find_element_by_name("byear").click()
         driver.find_element_by_name("byear").clear()
-        driver.find_element_by_name("byear").send_keys("1980")
+        driver.find_element_by_name("byear").send_keys(contact.byear)
         driver.find_element_by_name("aday").click()
-        Select(driver.find_element_by_name("aday")).select_by_visible_text("18")
+        Select(driver.find_element_by_name("aday")).select_by_visible_text(contact.aday)
         driver.find_element_by_name("aday").click()
         driver.find_element_by_name("amonth").click()
-        Select(driver.find_element_by_name("amonth")).select_by_visible_text("July")
+        Select(driver.find_element_by_name("amonth")).select_by_visible_text(contact.amonth)
         driver.find_element_by_name("amonth").click()
         driver.find_element_by_name("ayear").click()
         driver.find_element_by_name("ayear").clear()
-        driver.find_element_by_name("ayear").send_keys("1998")
+        driver.find_element_by_name("ayear").send_keys(contact.ayear)
         driver.find_element_by_name("new_group").click()
         driver.find_element_by_name("theform").click()
         driver.find_element_by_name("address2").click()
         driver.find_element_by_name("address2").clear()
-        driver.find_element_by_name("address2").send_keys("Address")
+        driver.find_element_by_name("address2").send_keys(contact.address_2)
         driver.find_element_by_name("phone2").clear()
-        driver.find_element_by_name("phone2").send_keys("Home address")
+        driver.find_element_by_name("phone2").send_keys(contact.home_address)
         driver.find_element_by_name("notes").clear()
-        driver.find_element_by_name("notes").send_keys("Notes")
+        driver.find_element_by_name("notes").send_keys(contact.notes)
         # submit new contact creation
         driver.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
@@ -99,13 +101,13 @@ class NewUpdateContactAddressBook(unittest.TestCase):
         # init new contact creation
         driver.find_element_by_link_text("add new").click()
 
-    def login(self, driver):
+    def login(self, driver, username, password):
         # login
         driver.find_element_by_name("user").click()
         driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("user").send_keys(username)
         driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_name("pass").send_keys(password)
         driver.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_site(self, driver):
---
+++ contact.py
@@ -0,0 +1,29 @@
+class Contact:
+
+    def __init__(self, first, middle, last, nick, title, company, address, home_telephone,
+                           mobile_telephone, work_telephone, fax_telephone, email_1, email_2, email_3, home_page, bday,
+                           bmonth, byear, aday, amonth, ayear, address_2, home_address, notes):
+        self.first = first
+        self.middle = middle
+        self.last = last
+        self.nick = nick
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home_telephone = home_telephone
+        self.mobile_telephone = mobile_telephone
+        self.work_telephone = work_telephone
+        self.fax_telephone = fax_telephone
+        self.email_1 = email_1
+        self.email_2 = email_2
+        self.email_3 = email_3
+        self.home_page = home_page
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.address_2 = address_2
+        self.home_address = home_address
+        self.notes = notes
