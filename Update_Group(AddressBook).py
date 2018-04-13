6b3eb741a7024dd7aa820ad92598343260eee13a
DisconnectRZN <36986483+DisconnectRZN@users.noreply.github.com>
2018-04-13 12:26:32 +0300

    Фикстура

--- Update_Group(AddressBook).py
+++ Update_Group(AddressBook).py
@@ -1,100 +1,21 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
+
+import pytest
 from group import Group
-import unittest, time, re
+from application import Application
 
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-class NewGroupAddressBook(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
-        self.base_url = "https://www.katalon.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
-    
-    def test_new_group_address_book(self):
-        driver = self.driver
-        self.open_home_page(driver)
-        self.login(driver, username="admin", password="secret")
-        self.open_group_page(driver)
-        self.create_group(driver, Group(name="Group Name", header="Group header", footer="Group footer"))
-        self.return_to_groups_page(driver)
-        self.logout(driver)
+def test_new_group_address_book(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="Group Name", header="Group header", footer="Group footer"))
+    app.logout()
 
-    def test_empty_group_address_book(self):
-        driver = self.driver
-        self.open_home_page(driver)
-        self.login(driver, username="admin", password="secret")
-        self.open_group_page(driver)
-        self.create_group(driver, Group(name="", header="", footer=""))
-        self.return_to_groups_page(driver)
-        self.logout(driver)
-
-    def logout(self, driver):
-        # logout
-        driver.find_element_by_link_text("Logout").click()
-
-    def return_to_groups_page(self, driver):
-        # return to groups page
-        driver.find_element_by_link_text("group page").click()
-
-    def create_group(self, driver, group):
-        # init group creation
-        driver.find_element_by_name("new").click()
-        # fill gtoup form
-        driver.find_element_by_name("group_name").click()
-        driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys(group.name)
-        driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys(group.header)
-        driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        driver.find_element_by_name("submit").click()
-
-    def open_group_page(self, driver):
-        # open group page
-        driver.find_element_by_link_text("groups").click()
-
-    def login(self, driver, username, password):
-        # login
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys(username)
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys(password)
-        driver.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, driver):
-        # open home page
-        driver.get("http://localhost/addressbook/group.php")
-
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
-    def tearDown(self):
-        self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
-
-if __name__ == "__main__":
-    unittest.main()
+def test_empty_group_address_book(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
