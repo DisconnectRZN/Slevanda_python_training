bbdaafa388ebed804b3114dc1065e87a424178b9
DisconnectRZN <36986483+DisconnectRZN@users.noreply.github.com>
2018-04-17 17:43:02 +0300

    1

--- conftest.py
+++ conftest.py
@@ -5,5 +5,9 @@ from Slevanda_python_training.Fixture.application import Application
 @pytest.fixture(scope="session", autouse=True)
 def app(request):
     fixture = Application()
-    request.addfinalizer(fixture.destroy)
+    fixture.session.login(username="admin", password="secret")
+    def fin():
+        fixture.session.logout()
+        fixture.destroy()
+    request.addfinalizer(fin)
     return fixture
--- test/Update_Group(AddressBook).py
+++ test/Update_Group(AddressBook).py
@@ -2,11 +2,7 @@
 from Slevanda_python_training.Model.group import Group
 
 def test_new_group_address_book(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="Group Name", header="Group header", footer="Group footer"))
-    app.session.logout()
 
 def test_empty_group_address_book(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
